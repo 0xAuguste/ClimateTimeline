60adacb811cd861ccc40b114518974f98f876d02
0xAuguste <gusinator9@gmail.com>
2022-07-05 18:11:13 +0200

    Update custom files

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 var data = [
-	    {time: new Date(1977, 4,25), episode: 4, name: 'A New Hope'},
-	    {time: new Date(1980, 4,17), episode: 5, name: 'The Empire Strikes Back'},
-	    {time: new Date(1984, 4,25), episode: 6, name: 'Return of the Jedi'},
-	    {time: new Date(1999, 4,19), episode: 1, name: 'The Phantom Menace'},
-	    {time: new Date(2002, 4,16), episode: 2, name: 'Attack of the Clones'},
-	    {time: new Date(2005, 4,19), episode: 3, name: 'Revenge of the Sith'},
-	    {time: new Date(2015,11,18), episode: 7, name: 'The Force Awakens'},
-	    ];
+    {time: new Date(1977, 4,25), name: 'A New Hope', dir: 'left', col: 'royalblue'},
+    {time: new Date(1980, 4,17), name: 'The Empire Strikes Back', dir: 'right', col: 'royalblue'},
+    {time: new Date(1984, 4,25), name: 'Return of the Jedi', dir: 'left', col: 'gold'},
+    {time: new Date(1999, 4,19), name: 'The Phantom Menace', dir: 'right', col: 'gold'},
+    {time: new Date(2002, 4,16), name: 'Attack of the Clones', dir: 'left', col: '#777'},
+    {time: new Date(2005, 4,19), name: 'Revenge of the Sith', dir: 'right', col: 'salmon'},
+    {time: new Date(2015,11,18), name: 'The Force Awakens', dir: 'left', col: 'gold'},
+    ];
